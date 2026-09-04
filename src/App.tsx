cc2e58a7349e02699c3bd9ec109c66a175a0faec
mairadaianaolivarez-hub <mairadaianaolivarez@gmail.com>
2026-09-04 10:10:34 -0300

    Update from Cosmos Code

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,24 @@ import Recetas from './pages/Recetas';
 import Planes from './pages/Planes';
 import Blog from './pages/Blog';
 import Contacto from './pages/Contacto';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div style={{ minHeight: '100vh', backgroundColor: '#FAFAFA' }}>
+      <div
+        className="relative min-h-screen"
+        style={{ backgroundColor: '#FAFAFA' }}
+      >
+        {/* Fondo de pantalla fijo */}
+        <div
+          className="fixed inset-0 -z-10 h-full w-full bg-cover bg-center bg-no-repeat opacity-20"
+          style={{
+            backgroundImage: `url('https://fzfncffjekempswnjilr.supabase.co/storage/v1/object/public/cosmos-code-sites/_assets/Ws3HONfaS0dZKNnL4oNlI9ZGSV23/2d2e17-e36b-4321-a51f-77fab61d3ff7/40ca46ec3c7e09da6993c7ad.png')`,
+          }}
+          aria-hidden="true"
+        />
         <Header />
         <Routes>
           <Route path="/" element={<Inicio />} />
@@ -17,6 +30,8 @@ export default function App() {
           <Route path="/planes" element={<Planes />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/contacto" element={<Contacto />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </div>
     </BrowserRouter>
